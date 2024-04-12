0fe3f4d88a34f67d45e970ef6064b497227d815f
Naoki Takahashi / 電電猫猫 <43579605+nyanko3141592@users.noreply.github.com>
2024-04-12 17:59:25 +0900

    format

--- AXUIElementInspector/AppDelegate.swift
+++ AXUIElementInspector/AppDelegate.swift
@@ -1,177 +1,177 @@
 import Cocoa
 
-
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-  // OverlayWindowとInspectorWindowのインスタンスを生成
-  lazy var overlayWindow = OverlayWindow()
-  lazy var inspectorWindow = InspectorWindow()
+    // OverlayWindowとInspectorWindowのインスタンスを生成
+    lazy var overlayWindow = OverlayWindow()
+    lazy var inspectorWindow = InspectorWindow()
 
-     var activeApplicationName: String = "" {
-       didSet {
-         inspectorWindow.activeApplicationName = activeApplicationName
-       }
-     }
-  // 現在インスペクションが有効かどうかを管理するプロパティ
-  var isInspectingEnabled: Bool = false {
-    didSet {
-      // インスペクションの有効/無効状態をinspectorWindowに伝える
-      inspectorWindow.isInspectingEnabled = isInspectingEnabled
-    }
-  }
-
-  // CGEventTapを管理するプロパティ
-  var tap: CFMachPort!
-
-  func applicationDidFinishLaunching(_ aNotification: Notification) {
-    // InspectorWindowのinspectボタンクリック時の処理を設定
-    inspectorWindow.inspectButtonClicked = {
-      self.isInspectingEnabled.toggle()
-    }
-
-    // InspectorWindowのサイズと位置を設定
-    inspectorWindow.setContentSize(CGSize(width: 600, height: 800))
-    inspectorWindow.setPosition(vertical: .bottom, horizontal: .right)
-
-    // InspectorWindowを前面に表示
-    inspectorWindow.orderFront(nil)
-    
-    // アクセシビリティの権限チェックを行う
-    let trustedCheckOptionPrompt = kAXTrustedCheckOptionPrompt.takeRetainedValue() as NSString
-    let options = [trustedCheckOptionPrompt: true] as CFDictionary
-    if AXIsProcessTrustedWithOptions(options) {
-      // 権限がある場合はsetup()を呼び出す
-      setup()
-    } else {
-      // 権限がない場合は権限が付与されるまで待機し、付与されたらsetup()を呼び出す
-      waitPermisionGranted {
-        self.setup()
-      }
-    }
-  }
-
-  private func setup() {
-    // CGEventTapを作成し、マウスイベントを監視
-    let mask: CGEventMask = (1 << CGEventType.leftMouseDown.rawValue) | (1 << CGEventType.mouseMoved.rawValue)
-    tap = CGEvent.tapCreate(
-      tap: .cgSessionEventTap,
-      place: .headInsertEventTap,
-      options: .defaultTap,
-      eventsOfInterest: mask,
-      callback: { (proxy, type, event, refcon) in
-        if let observer = refcon {
-          let this = Unmanaged<AppDelegate>.fromOpaque(observer).takeUnretainedValue()
-
-          switch event.type {
-          case .leftMouseDown:
-            // インスペクション中に左クリックされた場合はインスペクションを無効化
-            if this.isInspectingEnabled {
-              this.isInspectingEnabled.toggle()
-              return nil
-            }
-          case .mouseMoved:
-            // マウス移動時の処理を行う
-            this.mouseMoved()
-          case .tapDisabledByTimeout:
-            // タップが無効になった場合は再度有効化
-            this.enableTap()
-          default:
-            break
-          }
+    var activeApplicationName: String = "" {
+        didSet {
+            inspectorWindow.activeApplicationName = activeApplicationName
         }
-        return Unmanaged.passUnretained(event)
-      },
-      userInfo: UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque())
-    )
-
-    // CGEventTapをRunLoopに追加
-    let runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, tap, 0)
-    CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
-
-    // CGEventTapを有効化
-    enableTap()
-  }
-
-  private func enableTap() {
-    // CGEventTapを有効化する
-    CGEvent.tapEnable(tap: tap, enable: true)
-  }
-
-  private func mouseMoved() {
-    // インスペクションが有効でない場合は処理を終了
-    guard isInspectingEnabled else {
-      return
+    }
+    // 現在インスペクションが有効かどうかを管理するプロパティ
+    var isInspectingEnabled: Bool = false {
+        didSet {
+            // インスペクションの有効/無効状態をinspectorWindowに伝える
+            inspectorWindow.isInspectingEnabled = isInspectingEnabled
+        }
     }
 
-    // システム全体のAXUIElementを取得
-    let systemWideElement = AXUIElementCreateSystemWide()
+    // CGEventTapを管理するプロパティ
+    var tap: CFMachPort!
 
-    // マウスの位置を取得
-    let mouseLocation = carbonScreenPointFromCocoaScreenPoint(NSEvent.mouseLocation)
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // InspectorWindowのinspectボタンクリック時の処理を設定
+        inspectorWindow.inspectButtonClicked = {
+            self.isInspectingEnabled.toggle()
+        }
 
-    var element: AXUIElement?
-    // マウスの位置にあるAXUIElementを取得
-    let copyElementError = AXUIElementCopyElementAtPosition(
-      systemWideElement,
-      Float(mouseLocation.x),
-      Float(mouseLocation.y),
-      &element
-    )
+        // InspectorWindowのサイズと位置を設定
+        inspectorWindow.setContentSize(CGSize(width: 600, height: 800))
+        inspectorWindow.setPosition(vertical: .bottom, horizontal: .right)
 
-    // エラーチェック
-    guard let element, copyElementError == .success else {
-      return
+        // InspectorWindowを前面に表示
+        inspectorWindow.orderFront(nil)
+
+        // アクセシビリティの権限チェックを行う
+        let trustedCheckOptionPrompt = kAXTrustedCheckOptionPrompt.takeRetainedValue() as NSString
+        let options = [trustedCheckOptionPrompt: true] as CFDictionary
+        if AXIsProcessTrustedWithOptions(options) {
+            // 権限がある場合はsetup()を呼び出す
+            setup()
+        } else {
+            // 権限がない場合は権限が付与されるまで待機し、付与されたらsetup()を呼び出す
+            waitPermisionGranted {
+                self.setup()
+            }
+        }
     }
 
-    var attributeValue: AnyObject?
-    // AXUIElementの"AXFrame"属性を取得
-    let attributeValueError = AXUIElementCopyAttributeValue(
-      element,
-      "AXFrame" as CFString,
-      &attributeValue
-    )
+    private func setup() {
+        // CGEventTapを作成し、マウスイベントを監視
+        let mask: CGEventMask =
+            (1 << CGEventType.leftMouseDown.rawValue) | (1 << CGEventType.mouseMoved.rawValue)
+        tap = CGEvent.tapCreate(
+            tap: .cgSessionEventTap,
+            place: .headInsertEventTap,
+            options: .defaultTap,
+            eventsOfInterest: mask,
+            callback: { (proxy, type, event, refcon) in
+                if let observer = refcon {
+                    let this = Unmanaged<AppDelegate>.fromOpaque(observer).takeUnretainedValue()
 
-    // エラーチェック
-    guard let attributeValue, attributeValueError == .success else {
-      return
+                    switch event.type {
+                    case .leftMouseDown:
+                        // インスペクション中に左クリックされた場合はインスペクションを無効化
+                        if this.isInspectingEnabled {
+                            this.isInspectingEnabled.toggle()
+                            return nil
+                        }
+                    case .mouseMoved:
+                        // マウス移動時の処理を行う
+                        this.mouseMoved()
+                    case .tapDisabledByTimeout:
+                        // タップが無効になった場合は再度有効化
+                        this.enableTap()
+                    default:
+                        break
+                    }
+                }
+                return Unmanaged.passUnretained(event)
+            },
+            userInfo: UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque())
+        )
+
+        // CGEventTapをRunLoopに追加
+        let runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, tap, 0)
+        CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
+
+        // CGEventTapを有効化
+        enableTap()
     }
 
-    let value = attributeValue as! AXValue
-
-    var rect = CGRect()
-    // AXValueからCGRectを取得
-    guard AXValueGetValue(value, .cgRect, &rect) else {
-      return
+    private func enableTap() {
+        // CGEventTapを有効化する
+        CGEvent.tapEnable(tap: tap, enable: true)
     }
 
-    var origin = cocoaScreenPointFromCarbonScreenPoint(rect.origin)
-    origin.y -= rect.height
+    private func mouseMoved() {
+        // インスペクションが有効でない場合は処理を終了
+        guard isInspectingEnabled else {
+            return
+        }
 
-    // OverlayWindowの位置とサイズを設定
-    overlayWindow.setFrameOrigin(origin)
-    overlayWindow.setContentSize(rect.size)
+        // システム全体のAXUIElementを取得
+        let systemWideElement = AXUIElementCreateSystemWide()
 
-    // OverlayWindowを前面に表示
-    overlayWindow.orderFront(nil)
+        // マウスの位置を取得
+        let mouseLocation = carbonScreenPointFromCocoaScreenPoint(NSEvent.mouseLocation)
 
-    // InspectorWindowにAXUIElementの情報を表示
-    inspectorWindow.elementTitle = title(of: element)
-    inspectorWindow.attributedText = inspect(element: element)
-  
-  let workspace = NSWorkspace.shared
-  let activeApplication = workspace.frontmostApplication
-  activeApplicationName = activeApplication?.localizedName ?? "Unknown"
-  }
+        var element: AXUIElement?
+        // マウスの位置にあるAXUIElementを取得
+        let copyElementError = AXUIElementCopyElementAtPosition(
+            systemWideElement,
+            Float(mouseLocation.x),
+            Float(mouseLocation.y),
+            &element
+        )
 
-  private func waitPermisionGranted(completion: @escaping () -> Void) {
-    // 0.3秒後に権限チェックを行い、権限がある場合はcompletion()を呼び出す
-    // 権限がない場合は再度waitPermisionGranted()を呼び出す
-    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-      if AXIsProcessTrusted() {
-        completion()
-      } else {
-        self.waitPermisionGranted(completion: completion)
-      }
+        // エラーチェック
+        guard let element, copyElementError == .success else {
+            return
+        }
+
+        var attributeValue: AnyObject?
+        // AXUIElementの"AXFrame"属性を取得
+        let attributeValueError = AXUIElementCopyAttributeValue(
+            element,
+            "AXFrame" as CFString,
+            &attributeValue
+        )
+
+        // エラーチェック
+        guard let attributeValue, attributeValueError == .success else {
+            return
+        }
+
+        let value = attributeValue as! AXValue
+
+        var rect = CGRect()
+        // AXValueからCGRectを取得
+        guard AXValueGetValue(value, .cgRect, &rect) else {
+            return
+        }
+
+        var origin = cocoaScreenPointFromCarbonScreenPoint(rect.origin)
+        origin.y -= rect.height
+
+        // OverlayWindowの位置とサイズを設定
+        overlayWindow.setFrameOrigin(origin)
+        overlayWindow.setContentSize(rect.size)
+
+        // OverlayWindowを前面に表示
+        overlayWindow.orderFront(nil)
+
+        // InspectorWindowにAXUIElementの情報を表示
+        inspectorWindow.elementTitle = title(of: element)
+        inspectorWindow.attributedText = inspect(element: element)
+
+        let workspace = NSWorkspace.shared
+        let activeApplication = workspace.frontmostApplication
+        activeApplicationName = activeApplication?.localizedName ?? "Unknown"
     }
-  }
+
+    private func waitPermisionGranted(completion: @escaping () -> Void) {
+        // 0.3秒後に権限チェックを行い、権限がある場合はcompletion()を呼び出す
+        // 権限がない場合は再度waitPermisionGranted()を呼び出す
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            if AXIsProcessTrusted() {
+                completion()
+            } else {
+                self.waitPermisionGranted(completion: completion)
+            }
+        }
+    }
 }
--- AXUIElementInspector/InspectorWindow.swift
+++ AXUIElementInspector/InspectorWindow.swift
@@ -1,172 +1,180 @@
 import AppKit
 
-
 class InspectorWindow: NSPanel {
-    
+
     var activeApplicationName: String = "" {
-       didSet {
-         activeApplicationNameLabel.stringValue = activeApplicationName
-       }
-     }
-    
-  var elementTitle: String {
-    get {
-      elementTitleLabel.stringValue
+        didSet {
+            activeApplicationNameLabel.stringValue = activeApplicationName
+        }
     }
-    set {
-      elementTitleLabel.stringValue = newValue
+
+    var elementTitle: String {
+        get {
+            elementTitleLabel.stringValue
+        }
+        set {
+            elementTitleLabel.stringValue = newValue
+        }
     }
-  }
 
-  var text: String {
-    get {
-      documentView.string
+    var text: String {
+        get {
+            documentView.string
+        }
+        set {
+            documentView.string = newValue
+        }
     }
-    set {
-      documentView.string = newValue
+
+    var attributedText: NSAttributedString {
+        get {
+            documentView.attributedString()
+        }
+        set {
+            documentView.textStorage?.setAttributedString(newValue)
+        }
     }
-  }
 
-  var attributedText: NSAttributedString {
-    get {
-      documentView.attributedString()
+    var isInspectingEnabled: Bool = false {
+        didSet {
+            // インスペクトボタンの状態をisInspectingEnabledに応じて更新
+            inspectButton.state = isInspectingEnabled ? .on : .off
+        }
     }
-    set {
-      documentView.textStorage?.setAttributedString(newValue)
+
+    var inspectButtonClicked: () -> Void = {}
+
+    // インスペクト対象の要素セクションのラベル
+    private let inspectedElementSectionLabel = NSTextField(labelWithString: "Inspected Element:")
+    // インスペクト対象の要素のタイトルラベル
+    private let elementTitleLabel = NSTextField(labelWithString: "")
+    // インスペクトボタン
+    private let inspectButton = NSButton(
+        image: NSImage(named: "crosshair")!, target: nil, action: nil)
+
+    // スクロール可能なテキストビュー
+    private let textView = NSTextView.scrollableTextView()
+    // テキストビューのドキュメントビュー
+    private var documentView: NSTextView {
+        textView.documentView as! NSTextView
     }
-  }
 
-  var isInspectingEnabled: Bool = false {
-    didSet {
-      // インスペクトボタンの状態をisInspectingEnabledに応じて更新
-      inspectButton.state = isInspectingEnabled ? .on : .off
-    }
-  }
-
-  var inspectButtonClicked: () -> () = {}
-
-  // インスペクト対象の要素セクションのラベル
-  private let inspectedElementSectionLabel = NSTextField(labelWithString: "Inspected Element:")
-  // インスペクト対象の要素のタイトルラベル
-  private let elementTitleLabel = NSTextField(labelWithString: "")
-  // インスペクトボタン
-  private let inspectButton = NSButton(image: NSImage(named: "crosshair")!, target: nil, action: nil)
-
-  // スクロール可能なテキストビュー
-  private let textView = NSTextView.scrollableTextView()
-  // テキストビューのドキュメントビュー
-  private var documentView: NSTextView {
-    textView.documentView as! NSTextView
-  }
-
-    
     private let activeApplicationNameLabel = NSTextField(labelWithString: "")
-    
-  init() {
-    super.init(
-      contentRect: .zero,
-      styleMask: [.titled, .closable, .resizable, .utilityWindow, .nonactivatingPanel],
-      backing: .buffered,
-      defer: false
-    )
 
-    // フローティングパネルに設定
-    isFloatingPanel = true
-    level = .floating
+    init() {
+        super.init(
+            contentRect: .zero,
+            styleMask: [.titled, .closable, .resizable, .utilityWindow, .nonactivatingPanel],
+            backing: .buffered,
+            defer: false
+        )
 
-    // フルスクリーン時にパネルを表示するように設定
-    collectionBehavior.insert(.fullScreenAuxiliary)
+        // フローティングパネルに設定
+        isFloatingPanel = true
+        level = .floating
 
-    let rootView = NSView()
-    contentView = rootView
+        // フルスクリーン時にパネルを表示するように設定
+        collectionBehavior.insert(.fullScreenAuxiliary)
 
-    // インスペクト対象の要素セクションのラベルを設定
-    inspectedElementSectionLabel.translatesAutoresizingMaskIntoConstraints = false
-    inspectedElementSectionLabel.font = NSFont.controlContentFont(ofSize: 12)
-    inspectedElementSectionLabel.textColor = NSColor.secondaryLabelColor
-    rootView.addSubview(inspectedElementSectionLabel)
+        let rootView = NSView()
+        contentView = rootView
 
-    // インスペクト対象の要素のタイトルラベルを設定
-    elementTitleLabel.translatesAutoresizingMaskIntoConstraints = false
-    elementTitleLabel.font = NSFont.boldSystemFont(ofSize: 12)
-    rootView.addSubview(elementTitleLabel)
+        // インスペクト対象の要素セクションのラベルを設定
+        inspectedElementSectionLabel.translatesAutoresizingMaskIntoConstraints = false
+        inspectedElementSectionLabel.font = NSFont.controlContentFont(ofSize: 12)
+        inspectedElementSectionLabel.textColor = NSColor.secondaryLabelColor
+        rootView.addSubview(inspectedElementSectionLabel)
 
-    // インスペクトボタンを設定
-    inspectButton.translatesAutoresizingMaskIntoConstraints = false
-    inspectButton.setContentHuggingPriority(.required, for: .horizontal)
-    inspectButton.bezelStyle = .flexiblePush
-    inspectButton.imageScaling = .scaleNone
-    inspectButton.setButtonType(.onOff)
+        // インスペクト対象の要素のタイトルラベルを設定
+        elementTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        elementTitleLabel.font = NSFont.boldSystemFont(ofSize: 12)
+        rootView.addSubview(elementTitleLabel)
 
-    inspectButton.target = self
-    inspectButton.action = #selector(inspectButtonAction)
+        // インスペクトボタンを設定
+        inspectButton.translatesAutoresizingMaskIntoConstraints = false
+        inspectButton.setContentHuggingPriority(.required, for: .horizontal)
+        inspectButton.bezelStyle = .flexiblePush
+        inspectButton.imageScaling = .scaleNone
+        inspectButton.setButtonType(.onOff)
 
-    rootView.addSubview(inspectButton)
+        inspectButton.target = self
+        inspectButton.action = #selector(inspectButtonAction)
 
-    // セパレーターを作成
-    let separator = NSBox()
-    separator.translatesAutoresizingMaskIntoConstraints = false
-    separator.boxType = .separator
-    rootView.addSubview(separator)
+        rootView.addSubview(inspectButton)
 
-    // テキストビューを設定
-    textView.translatesAutoresizingMaskIntoConstraints = false
-    textView.scrollerStyle = .overlay
+        // セパレーターを作成
+        let separator = NSBox()
+        separator.translatesAutoresizingMaskIntoConstraints = false
+        separator.boxType = .separator
+        rootView.addSubview(separator)
 
-    documentView.drawsBackground = false
-    documentView.isEditable = false
-    documentView.isSelectable = true
-    documentView.textContainerInset = NSSize(width: 8, height: 0)
-    documentView.textContainer?.widthTracksTextView = false
+        // テキストビューを設定
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.scrollerStyle = .overlay
 
-    rootView.addSubview(textView)
-      
-      activeApplicationNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        documentView.drawsBackground = false
+        documentView.isEditable = false
+        documentView.isSelectable = true
+        documentView.textContainerInset = NSSize(width: 8, height: 0)
+        documentView.textContainer?.widthTracksTextView = false
+
+        rootView.addSubview(textView)
+
+        activeApplicationNameLabel.translatesAutoresizingMaskIntoConstraints = false
         activeApplicationNameLabel.font = NSFont.systemFont(ofSize: 12)
         activeApplicationNameLabel.textColor = NSColor.secondaryLabelColor
         rootView.addSubview(activeApplicationNameLabel)
-        
+
         NSLayoutConstraint.activate([
-          activeApplicationNameLabel.topAnchor.constraint(equalTo: inspectedElementSectionLabel.topAnchor),
-          activeApplicationNameLabel.trailingAnchor.constraint(equalTo: inspectButton.leadingAnchor, constant: -12),
+            activeApplicationNameLabel.topAnchor.constraint(
+                equalTo: inspectedElementSectionLabel.topAnchor),
+            activeApplicationNameLabel.trailingAnchor.constraint(
+                equalTo: inspectButton.leadingAnchor, constant: -12),
         ])
-    // Auto Layoutを設定
-    NSLayoutConstraint.activate([
-      inspectedElementSectionLabel.topAnchor.constraint(equalTo: rootView.topAnchor, constant: 12),
-      inspectedElementSectionLabel.leadingAnchor.constraint(equalTo: rootView.leadingAnchor, constant: 12),
-      inspectedElementSectionLabel.trailingAnchor.constraint(equalTo: inspectButton.leadingAnchor, constant: -12),
+        // Auto Layoutを設定
+        NSLayoutConstraint.activate([
+            inspectedElementSectionLabel.topAnchor.constraint(
+                equalTo: rootView.topAnchor, constant: 12),
+            inspectedElementSectionLabel.leadingAnchor.constraint(
+                equalTo: rootView.leadingAnchor, constant: 12),
+            inspectedElementSectionLabel.trailingAnchor.constraint(
+                equalTo: inspectButton.leadingAnchor, constant: -12),
 
-      elementTitleLabel.topAnchor.constraint(equalTo: inspectedElementSectionLabel.bottomAnchor, constant: 2),
-      elementTitleLabel.leadingAnchor.constraint(equalTo: rootView.leadingAnchor, constant: 12),
-      elementTitleLabel.trailingAnchor.constraint(equalTo: inspectButton.leadingAnchor, constant: -8),
+            elementTitleLabel.topAnchor.constraint(
+                equalTo: inspectedElementSectionLabel.bottomAnchor, constant: 2),
+            elementTitleLabel.leadingAnchor.constraint(
+                equalTo: rootView.leadingAnchor, constant: 12),
+            elementTitleLabel.trailingAnchor.constraint(
+                equalTo: inspectButton.leadingAnchor, constant: -8),
 
-      inspectButton.topAnchor.constraint(equalTo: rootView.topAnchor, constant: 12),
-      inspectButton.trailingAnchor.constraint(equalTo: rootView.trailingAnchor, constant: -12),
-      inspectButton.widthAnchor.constraint(equalToConstant: 32),
-      inspectButton.heightAnchor.constraint(equalToConstant: 30),
+            inspectButton.topAnchor.constraint(equalTo: rootView.topAnchor, constant: 12),
+            inspectButton.trailingAnchor.constraint(
+                equalTo: rootView.trailingAnchor, constant: -12),
+            inspectButton.widthAnchor.constraint(equalToConstant: 32),
+            inspectButton.heightAnchor.constraint(equalToConstant: 30),
 
-      separator.topAnchor.constraint(equalTo: elementTitleLabel.bottomAnchor, constant: 12),
-      separator.leadingAnchor.constraint(equalTo: rootView.leadingAnchor, constant: 12),
-      separator.trailingAnchor.constraint(equalTo: rootView.trailingAnchor, constant: -12),
+            separator.topAnchor.constraint(equalTo: elementTitleLabel.bottomAnchor, constant: 12),
+            separator.leadingAnchor.constraint(equalTo: rootView.leadingAnchor, constant: 12),
+            separator.trailingAnchor.constraint(equalTo: rootView.trailingAnchor, constant: -12),
 
-      textView.topAnchor.constraint(equalTo: separator.bottomAnchor, constant: 10),
-      textView.leadingAnchor.constraint(equalTo: rootView.leadingAnchor, constant: 0),
-      textView.trailingAnchor.constraint(equalTo: rootView.trailingAnchor, constant: 0),
-      textView.bottomAnchor.constraint(equalTo: rootView.bottomAnchor, constant: 0),
-    ])
-  }
+            textView.topAnchor.constraint(equalTo: separator.bottomAnchor, constant: 10),
+            textView.leadingAnchor.constraint(equalTo: rootView.leadingAnchor, constant: 0),
+            textView.trailingAnchor.constraint(equalTo: rootView.trailingAnchor, constant: 0),
+            textView.bottomAnchor.constraint(equalTo: rootView.bottomAnchor, constant: 0),
+        ])
+    }
 
-  override var canBecomeKey: Bool {
-    return true
-  }
+    override var canBecomeKey: Bool {
+        return true
+    }
 
-  override var canBecomeMain: Bool {
-    return true
-  }
+    override var canBecomeMain: Bool {
+        return true
+    }
 
-  @objc
-  func inspectButtonAction() {
-    // インスペクトボタンがクリックされた時の処理を呼び出す
-    inspectButtonClicked()
-  }
+    @objc
+    func inspectButtonAction() {
+        // インスペクトボタンがクリックされた時の処理を呼び出す
+        inspectButtonClicked()
+    }
 }
